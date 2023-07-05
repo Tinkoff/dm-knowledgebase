2fee2a9ffcb7793137d4ff97051b9607bd44ff98
v.shitov <v.shitov@tinkoff.ru>
2023-07-05 12:59:57 +0300

    style: Новый дизайн v1.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   baseUrl: '/dm-knowledgebase/',
   onBrokenLinks: 'log',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   organizationName: 'Tinkoff', // Usually your GitHub org/user name.
   projectName: 'dm-knowledgebase', // Usually your repo name.
   trailingSlash: false,
